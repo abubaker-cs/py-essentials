a9df7c354ff39dcc681ab32fa1f984783eb9dd20
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-06-15 14:52:50 +0500

    Studied about using the .format() function with print() statement.

---
+++ Chap02/format.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+x = 42
+print('Hello, World. {}'.format(x))
+print(f'Hello, Py 3.6+ you can use this {x}')
+
+# PY 2
+print('Hello, coder . %d' % x)
